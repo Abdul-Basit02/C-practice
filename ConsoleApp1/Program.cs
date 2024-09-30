3cecfe19fd1371f01482673777cbe3ebd6c41f85
Abdul-Basit02 <shahidmajeed6543@gmail.com>
2024-09-30 19:32:25 +0500

    practice complete

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 20;
+            Console.WriteLine(age);
+            string mytext = "basit";
+            Console.WriteLine(mytext);
+            string email = "basit@gmail.com";
+            Console.WriteLine(email);
+            string pass = "123";
+            Console.WriteLine(pass);
+            Console.WriteLine("hello, world!");
+            Console.WriteLine("debug, frame!");
+            Console.WriteLine("name, zero!");
+            Console.ReadKey();
+        }
+    }
+}
